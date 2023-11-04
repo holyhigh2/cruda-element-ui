216e3b64d35fe90be510d6dd38fdedf3641f6fb4
holyhigh2 <ylem@foxmail.com>
2023-11-05 00:18:09 +0800

    v 1.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.3.0] - 2023/11/4
+### Add
+- support `recoverable`
+
 ## [1.1.0] - 2023/7/15 ⚠️BreakChange
 ### Change
 - Switch the func-lib to myfx
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,60 @@
 /**
  * @author holyhigh
  */
-import { each } from 'myfx/collection'
-import {closest} from 'myfx/tree'
-import { noop } from 'myfx/utils'
-import { get } from 'myfx/object'
+import { each } from "myfx/collection";
+import { closest } from "myfx/tree";
+import { noop } from "myfx/utils";
+import { get } from "myfx/object";
 
+import CRUD, { crudError, RestUrl, _newCrud, _newCruds, _onHook,_setUpdater,_setSnapshot } from "cruda";
+import * as packageInfo from "../package.json";
 
-import CRUD, { crudError, RestUrl,_newCrud,_newCruds, _onHook} from 'cruda'
-import * as packageInfo from '../package.json'
+let globalVue: Record<string, any>;
 
-let globalVue: Record<string, any>
+function watchCrud(
+  crudInstances: CRUD | Record<string, CRUD>,
+  vm: Record<string, any>
+) {
+  let crud = globalVue.observable(crudInstances);
+
+  //check recoverable
+  if (CRUD.defaults.recoverable) {
+    //测试直接监控crud对象里的form属性
+    if (crudInstances instanceof CRUD) {
+      vm.$watch(
+        () => {
+          return crud.form;
+        },
+        (nv:any) => {
+          if(crudInstances.formStatus !== 1 && crudInstances.formStatus !== 2){
+            return
+          }
+
+          _setSnapshot(crud,nv)
+        },
+        { deep: true }
+      );
+    }else {
+      each(crudInstances,(crud,k)=>{
+        vm.$watch(
+          () => {
+            return crud.form;
+          },
+          (nv:any) => {
+            if(crud.formStatus !== 1 && crud.formStatus !== 2){
+              return
+            }
+  
+            _setSnapshot(crud,nv)
+          },
+          { deep: true }
+        );
+      })
+    }
+  }
+
+  return crud;
+}
 
 /**
  * 创建一个crud单实例入口并绑定指定的vm(需要crud的vue组件)
@@ -22,12 +66,12 @@ export function useCrud(
   vm: Record<string, any>,
   restURL: string | RestUrl
 ): CRUD {
-  const $crud = globalVue.observable(_newCrud(restURL,vm))
+  const $crud = watchCrud(_newCrud(restURL, vm), vm);
 
   //crud入口标识
-  vm.$isCrudEntry = true
+  vm.$isCrudEntry = true;
 
-  return $crud as CRUD
+  return $crud as CRUD;
 }
 
 /**
@@ -40,12 +84,12 @@ export function useCruds(
   vm: Record<string, any>,
   restURL: Record<string, string | RestUrl>
 ): Record<string, CRUD> {
-  const $cruds: Record<string, CRUD> = globalVue.observable(_newCruds(restURL,vm))
+  const $cruds: Record<string, CRUD> = watchCrud(_newCruds(restURL, vm), vm);
 
   //crud入口标识
-  vm.$isCrudEntry = true
+  vm.$isCrudEntry = true;
 
-  return $cruds
+  return $cruds;
 }
 
 /**
@@ -58,10 +102,14 @@ export function onHook(
   vm: Record<string, any>,
   hookName: string,
   hook: (crud: CRUD, ...args: any[]) => void
-): ()=>void {
-  let crudVM = closest(vm,(node: Record<any, any>)=>!!node.__crud_nid_,'$parent')
-  if(!crudVM)return noop
-  return _onHook(crudVM.__crud_nid_,hookName,hook,vm)
+): () => void {
+  let crudVM = closest(
+    vm,
+    (node: Record<any, any>) => !!node.__crud_nid_,
+    "$parent"
+  );
+  if (!crudVM) return noop;
+  return _onHook(crudVM.__crud_nid_, hookName, hook, vm);
 }
 
 /**
@@ -75,80 +123,94 @@ export function lookUpCrud(
   vm: Record<string, any>,
   crudName?: string
 ): CRUD | null {
-  let crudInstance = closest(vm, (node: Record<any, any>) => node.$crud || node.$cruds,'$parent')
-  if (!crudInstance) return crudInstance
+  let crudInstance = closest(
+    vm,
+    (node: Record<any, any>) => node.$crud || node.$cruds,
+    "$parent"
+  );
+  if (!crudInstance) return crudInstance;
 
-  let crudEntry = closest(vm, (node: Record<any, any>) => !!node.__crud_nid_, '$parent')
+  let crudEntry = closest(
+    vm,
+    (node: Record<any, any>) => !!node.__crud_nid_,
+    "$parent"
+  );
 
   //onHook
-  const methods = get<Record<string, any>>(vm,'constructor.options.methods')
-  each(CRUD.HOOK, hookName => {
+  const methods = get<Record<string, any>>(vm, "constructor.options.methods");
+  methods && each(CRUD.HOOK, (hookName) => {
     if (methods[hookName]) {
-      _onHook(crudEntry!.__crud_nid_, hookName, methods[hookName], vm)
+      _onHook(crudEntry!.__crud_nid_, hookName, methods[hookName], vm);
     }
-  })
+  });
 
-  if (crudInstance.$cruds){
+  if (crudInstance.$cruds) {
     if (!crudName) {
-      crudError(`Must specify 'crudName' when multiple instances detected`)
-      return null
+      crudError(`Must specify 'crudName' when multiple instances detected`);
+      return null;
     }
-    return crudInstance.$cruds[crudName]
+    return crudInstance.$cruds[crudName];
   }
 
-  return crudInstance.$crud
+  return crudInstance.$crud;
 }
 
 CRUD.install = function (Vue: Record<string, any>, options) {
   // 使用业务主请求器，避免因跨域或token等导致的异步失败
   if (!options.request) {
-    crudError('Cannot find [request] in the installation options')
+    crudError("Cannot find [request] in the installation options");
   }
-  CRUD.request = options.request
-  globalVue = Vue
+  CRUD.request = options.request;
+  globalVue = Vue;
+
+  _setUpdater((form:Record<string, any>, props:Record<string, any>)=>{
+    each(props,(v,k)=>{
+      Vue.set(form,k,v)
+    })    
+  })
 
   // 注入curd入口。
   // 以此简化在每个使用场景需要引入CRUD的过程
   Vue.mixin({
     beforeCreate() {
       // 具有标记crud的组件认为该组件启用了crud，即可注入$crud属性
-      const crud = this.$options.crud
-      const cruds = this.$options.cruds
+      const crud = this.$options.crud;
+      const cruds = this.$options.cruds;
       if (crud || cruds) {
         if (crud) {
-          this.$crud = Vue.observable(_newCrud(crud,this))
+          this.$crud = watchCrud(_newCrud(crud, this), this);
         } else {
-          this.$cruds = Vue.observable(_newCruds(cruds,this))
+          this.$cruds = watchCrud(_newCruds(cruds, this), this);
         }
         //crud入口标识
-        this.$isCrudEntry = true
+        this.$isCrudEntry = true;
 
         //onHook
-        const methods = this.constructor.options.methods
-        each(CRUD.HOOK,hookName=>{
-          if(methods[hookName]){
-            _onHook(this.__crud_nid_,hookName,methods[hookName],this)
+        const methods = this.constructor.options.methods;
+        methods && each(CRUD.HOOK, (hookName) => {
+          if (methods[hookName]) {
+            _onHook(this.__crud_nid_, hookName, methods[hookName], this);
           }
-        })
+        });
       }
     },
-  })
+  });
 
   // welcome info
-  const ssAry: string[] = []
-  ;['220,235,153', '179,208,75', '153,189,37'].forEach((v, i) => {
-    const cu = 'background:rgb(' + v + ');'
+  const ssAry: string[] = [];
+  ["220,235,153", "179,208,75", "153,189,37"].forEach((v, i) => {
+    const cu = "background:rgb(" + v + ");";
     if (i < 2) {
-      ssAry[i] = ssAry[5 - 1 - i] = cu
+      ssAry[i] = ssAry[5 - 1 - i] = cu;
     } else {
-      ssAry[i] = 'color:#fff;' + cu
+      ssAry[i] = "color:#fff;" + cu;
     }
-  })
+  });
   console.info(
     `%c %c %c Cruda/element-ui - ${packageInfo.description} | v${packageInfo.version} %c %c `,
     ...ssAry,
-    '\u{1F4E6} https://github.com/holyhigh2/cruda-element-ui'
-  )
-}
+    "\u{1F4E6} https://github.com/holyhigh2/cruda-element-ui"
+  );
+};
 
-export default CRUD
+export default CRUD;
